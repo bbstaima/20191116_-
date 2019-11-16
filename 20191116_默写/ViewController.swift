061c20af5341955f7caca15faef2c5eed25ce32c
Lei Shi <414033088@qq.com>
2019-11-16 18:54:07 +0800

    Initial Commit

---
+++ 20191116_默写/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20191116_默写
+//
+//  Created by apple on 2019/11/16.
+//  Copyright © 2019 newTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+
